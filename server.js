03c5bf858766bfd35449a1dba016ea41f4969362
Ali Diouri <alidiouri@gmail.com>
2016-09-23 00:45:22 +0100

    adding html page

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.send("Hello dev, am a local route nobody knows me yet!");
-});
+app.use(express.static(__dirname + "/public"))
 
 var server = app.listen(port, function () {
     console.log('node server is just fine! and running on port - ' + port);
