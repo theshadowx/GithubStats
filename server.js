c0889919901c387419cdc13f2d88e6e7834aedbf
Ali Diouri <alidiouri@gmail.com>
2016-09-24 16:36:37 +0100

    verify db connection before using it

--- server.js
+++ server.js
@@ -11,51 +11,62 @@ var io = socketio.listen(server);
 // MongoDB
 var databaseurl = "172.17.0.1:27017/contactList"
 var mongojs = require('mongojs');
-var db = mongojs('contactList', ['contactList']);
+var db = mongojs('contactList', ['contactList'])
 // body-parser
 var bodyParser = require('body-parser');
 
 
-function addPersonToContactList(nameStr, emailStr, numberStr)
-{
-    db.contactList.save({name: nameStr, email: emailStr, number: numberStr}, function(err, saved) {
-        if( err || !saved ) console.log("User not saved");
-        else console.log("User saved");
-    }); 
-}
+db.on('error', function (err) {
+    console.log('database error', err)
+})
 
-db.contactList.count(function (err, count) {
-    if (!err && count === 0) {
-        addPersonToContactList("Ali", "ali@gmail.com", "111-111-1111");
-        addPersonToContactList("Tom", "tom@gmail.com", "222-222-2222");
-        addPersonToContactList("Jane", "jane@gmail.com", "333-333-3333");
-        addPersonToContactList("Ahmed", "ahmed@gmail.com", "444-444-4444");
+db.on('connect', function () {
+    console.log('database connected')
+
+    function addPersonToContactList(nameStr, emailStr, numberStr)
+    {
+        db.contactList.save({name: nameStr, email: emailStr, number: numberStr}, function(err, saved) {
+            if( err || !saved ) console.log("User not saved");
+            else console.log("User saved");
+        }); 
     }
-});
+
+    db.contactList.count(function (err, count) {
+        if (!err && count === 0) {
+            addPersonToContactList("Ali", "ali@gmail.com", "111-111-1111");
+            addPersonToContactList("Tom", "tom@gmail.com", "222-222-2222");
+            addPersonToContactList("Jane", "jane@gmail.com", "333-333-3333");
+            addPersonToContactList("Ahmed", "ahmed@gmail.com", "444-444-4444");
+        }
+    });
+
+    // get
+    app.get('/containerList', function(req, res){
+        console.log("received a GET request");
+
+        db.contactList.find(function(err, docs){
+            res.json(docs);
+        });
+    });
+
+    io.on('connection', function (socket) {
+        console.log("new socket connected");
+        
+        socket.on('contactAdded', function (data) {
+            console.log(JSON.stringify(data, null,'\t'));
+            if(data){
+                db.contactList.insert(data);
+                io.emit('updateContactAdded', data);
+            }
+        });
+    });
+})
+
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-// get
-app.get('/containerList', function(req, res){
-    console.log("received a GET request");
 
-    db.contactList.find(function(err, docs){
-        res.json(docs);
-    });
-});
-
-io.on('connection', function (socket) {
-    console.log("new socket connected");
-    
-    socket.on('contactAdded', function (data) {
-        console.log(JSON.stringify(data, null,'\t'));
-        if(data){
-            db.contactList.insert(data);
-            io.emit('updateContactAdded', data);
-        }
-    });
-});
 
 // start server
 server.listen(port, function(){
