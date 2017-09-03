bb0696c188524fe47f766bf4505c5756e13830cd
Ali Diouri <alidiouri@gmail.com>
2017-09-03 21:07:37 +0100

    Monitoring Github Notes Stats

--- public/Controller/controller.js
+++ public/Controller/controller.js
@@ -1,10 +1,13 @@
 'use strict';
 
-var myApp = angular.module('myApp',['ngMaterial']);
+var myApp = angular.module('myApp',['ngMaterial','ngRoute', 'restangular']);
 
 myApp.config(function($mdThemingProvider) {
     $mdThemingProvider.theme('input');
-    $mdThemingProvider.theme('docs-dark', 'default')
+    $mdThemingProvider.theme('docs-dark', 'default');
+
+}).config(function(RestangularProvider) {
+    RestangularProvider.setBaseUrl('https://api.github.com/repos/nuttyartist/notes/releases/');
 });
 
 myApp.factory('socket', ['$rootScope', function($rootScope) {
@@ -12,9 +15,14 @@ myApp.factory('socket', ['$rootScope', function($rootScope) {
     return socket;
 }]);
 
-
-myApp.controller('AppCtrl', ['$scope', '$http', 'socket', '$timeout', '$mdSidenav', function($scope, $http, socket, $timeout, $mdSidenav){
+myApp.controller('AppCtrl', ['$scope', '$http', 'socket', '$timeout', '$mdSidenav', 'Restangular', function($scope, $http, socket, $timeout, $mdSidenav, Restangular){
     console.log("hi there");
+
+    
+
+    $scope.repoName = "Notes";
+    $scope.releaseTag = "v1.0.0";
+
 
     $scope.toggleLeft = buildToggler('left');
     $scope.toggleRight = buildToggler('right');
@@ -25,31 +33,181 @@ myApp.controller('AppCtrl', ['$scope', '$http', 'socket', '$timeout', '$mdSidena
       }
     }
 
-    $scope.contactList=[];
 
-    socket.on('updateContactAdded', function(data) {
-        console.log("updateContactAdded : " + JSON.stringify(data));
-        $scope.contactList.push(data);
-        $scope.$apply();
+    var updateChartHour = function(data){
+        var dataLength = data.length - 2;
+        var i = dataLength;
+    
+        data.forEach(function(element){
+            if(i !== -1){
+                myChartHour.data.datasets[0].data[i] = element.minuteTotal;
+                i--;
+            }
+            
+        });
+        myChartHour.update();
+    };
 
+    var updateChartDay = function(data){
+        var dataLength = data.length - 1;
+        var i = dataLength;
+    
+        data.forEach(function(element){
+            if(i !== -1){
+                myChartDay.data.datasets[0].data[i] = element.hourTotal;
+                i--;
+            }
+            
+        });
+        myChartDay.update();
+    };
+
+    var updateChartTotal = function(data){
+        if(data.length > 0) {
+            myChart.data.datasets[0].data = [data[0].macCnt, data[0].windowsCnt, data[0].linuxCnt, data[0].totalCnt];
+        }else{
+            myChart.data.datasets[0].data = [0, 0, 0, 0];
+        }
+        myChart.update();
+    };
+
+
+    socket.on('updateRecordTotal', function(data) {
+        //console.log("Total ****************************");
+        //console.log(data);
+        updateChartTotal(data);
+    });
+  
+
+    socket.on('updateRecordHour', function(data) {
+        //console.log("Hour ****************************");
+        //console.log(data);
+        updateChartHour(data);
+        //updateTotal(data);
     });
 
-    // get
-    var refresh = function(){
-        $http.get('/containerList').success(function(response){
-            console.log("got data");
-            $scope.contactList = response;
-            $scope.contact = "";
-        });
-    }
+    socket.on('updateRecordDay', function(data) {
+        //console.log("Day ****************************");
+        //console.log(data);
+        updateChartDay(data);
+    });
 
-    refresh();
 
-    // post
-    $scope.addContact = function(){
-        console.log($scope.contact);
-        socket.emit('contactAdded', $scope.contact);
-        $scope.contact = "";
-    };
+    var countAll = 0;
+    var countLinux = 0;
+    var countWindows = 0;
+    var countMac = 0;
+
+    var ctx = document.getElementById("githubChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Mac", "Windows", "Linux", "Total"],
+            datasets: [{
+                label: '# of downloads',
+                data: [countMac, countWindows, countLinux, countAll],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+
+    var ctxHour = document.getElementById("githubChartHour").getContext('2d');
+    var myChartHour = new Chart(ctxHour, {
+        type: 'bar',
+        data: {
+            labels: [60,59,58,57,56,
+                     55,54,53,52,51,
+                     50,49,48,47,46,
+                     45,44,43,42,41,
+                     40,39,38,37,36,
+                     35,44,43,32,31,
+                     30,29,28,27,26,
+                     25,24,23,22,21,
+                     20,19,18,17,16,
+                     15,14,13,12,11,
+                     10,9,8,7,6,
+                     5,4,3,2,1],
+            datasets: [{
+                label: '# of downloads / minute',
+                data: [0,0,0,0,0,
+                       0,0,0,0,0,
+                       0,0,0,0,0,
+                       0,0,0,0,0,
+                       0,0,0,0,0,
+                       0,0,0,0,0,
+                       0,0,0,0,0,
+                       0,0,0,0,0,
+                       0,0,0,0,0,
+                       0,0,0,0,0,
+                       0,0,0,0,0,
+                       0,0,0,0,0],
+                backgroundColor: 'rgba(104, 171, 89,1)',
+                borderWidth: 0
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }],
+                xAxes: [{
+                    display: false
+                }]
+            }
+        }
+    });
+
+    var ctxDay = document.getElementById("githubChartDay").getContext('2d');
+    var myChartDay = new Chart(ctxDay, {
+        type: 'bar',
+        data: {
+            labels: [24,23,22,21,20,19,18,17,
+                     16,15,14,13,12,11,10,9,
+                     8,7,6,5,4,3,2,1],
+            datasets: [{
+                label: '# of downloads',
+                data: [0,0,0,0,0,0,0,0,
+                       0,0,0,0,0,0,0,0,
+                       0,0,0,0,0,0,0,0],
+                backgroundColor: 'rgba(94, 142, 200,1)',
+                borderWidth: 0
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }],
+                xAxes: [{
+                    display: false
+                }]
+            }
+        }
+    });
 
 }]);
--- server.js
+++ server.js
@@ -5,26 +5,230 @@ var app = express();
 // http
 var http = require('http')
 var server = http.Server(app)
+//request
+var rp = require('request-promise');
 // socket.io
 var socketio = require('socket.io');
 var io = socketio.listen(server);
 // MongoDB Docker 
-var databaseurl = process.env.HEROKUDB || "172.17.0.1:27017/contactList?autoReconnect=true";
+// docker run -d -p 27017:27017 -p 28017:28017 -e AUTH=no tutum/mongodb
+var databaseurl = process.env.HEROKUDB || "172.17.0.1:27017/notesReleaseStat?autoReconnect=true";
 var mongojs = require('mongojs');
 var db = mongojs(databaseurl)
 // body-parser
 var bodyParser = require('body-parser');
 
-function addContact(nameStr, emailStr, numberStr)
+const user = "nuttyartist";
+const repo = "notes";
+const releaseTag = "v1.0.0";
+const hostUrl = 'https://api.github.com/repos/'+ user + '/' + repo + '/releases/tags/' + releaseTag;
+
+var previousTotalValue   = 0;
+var previousWindowsValue = 0;
+var previousLinuxValue   = 0;
+var previousMacValue     = 0;
+
+function getDataFromGithub(){
+
+    var options = {
+        uri: hostUrl,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
+
+    var timestamp = Math.floor(Date.now()/60000) * 60000;
+
+    rp(options)
+    .then(function (release) {
+        var countAll     = 0;
+        var countLinux   = 0;
+        var countWindows = 0;
+        var countMac     = 0;
+        
+        release.assets.forEach(function(installer){
+                
+            var fileName = JSON.stringify(installer.name);
+
+            if(fileName.indexOf(".dmg") !== -1){
+                countMac = installer.download_count;
+            }else if(fileName.indexOf(".exe")!== -1){
+                countWindows = installer.download_count;
+            }else if(fileName.indexOf(".deb")!== -1||
+                fileName.indexOf(".snap")!== -1 ||
+                fileName.indexOf(".AppImage")!== -1 ){
+                countLinux += installer.download_count;
+            }
+        });
+
+        countAll = countMac + countWindows + countLinux;
+
+        previousTotalValue   = countAll;
+        previousWindowsValue = countWindows;
+        previousLinuxValue   = countLinux;
+        previousMacValue     = countMac;
+
+        addRecord(timestamp, releaseTag, countWindows, countMac, countLinux, countAll);
+    })
+    .catch(function (err) {
+        var timestamp = Math.floor(Date.now()/60000) * 60000;
+        addRecord(timestamp, releaseTag, previousWindowsValue, previousMacValue, previousLinuxValue, previousTotalValue);
+        
+        console.log("** ERROR : ", err.message);
+        // API call failed...
+    });
+}
+
+function addRecord(timestampValue, versionValue, windowsCntValue, macCntValue, linuxCntValue, totalCntValue)
 {
-    contactList.save({name: nameStr, email: emailStr, number: numberStr}, function(err, saved) {
-        if( err || !saved ) console.log("User not saved");
-        else console.log("User saved");
+    notesReleaseStat.save({
+        timestamp:  timestampValue, 
+        version:    versionValue, 
+        windowsCnt: windowsCntValue, 
+        macCnt:     macCntValue,
+        linuxCnt:   linuxCntValue,
+        totalCnt:   totalCntValue}
+        , function(err, saved) {
+            if( err || !saved ) console.log("User not saved");
+            //else console.log("record saved : ", timestampValue, new Date(timestampValue).toTimeString(),totalCntValue);
     }); 
 }
 
-db.createCollection("contactList", {});
-var contactList = db.collection('contactList');
+
+function sendTotal(socket){
+
+    var totalRecord = [];
+
+    notesReleaseStat.find().sort({"timestamp":-1}).toArray(function (err, items) {
+        if(err){
+            console("** ERROR [sendTotal]: ", err.message);
+            return;
+        }else{
+            if(items.length > 0){
+                totalRecord.push(items[0]);
+            }            
+        }
+
+        socket.emit('updateRecordTotal', totalRecord);
+
+    });
+}
+
+
+function sendHourValues(socket){
+    var dateNow = Math.floor(Date.now()/60000) * 60000;
+    var timestampMin = dateNow - (60*61*1000)
+
+    var hourItems = [];
+
+    notesReleaseStat.find({ timestamp: {$gte: timestampMin } }).sort({"timestamp":-1}).limit(61).toArray(function (err, items) {
+                
+        for(var i=1; i<61; i++){
+            var minuteRangeMin =  dateNow - (60*i*1000);
+            var minuteRangeMax =  dateNow - (60*(i-1)*1000);
+
+            var diffMinuteWindowsValue = 0;
+            var diffMinuteMacValue     = 0;
+            var diffMinuteLinuxValue   = 0;
+            var diffMinuteTotalValue   = 0;
+
+            if(err){
+                console("** ERROR [sendHourValues]: ", err.message);
+
+            }else{
+
+                var allMinuteItems;
+
+                if(items.length > 0){
+
+                    allMinuteItems = items.filter(function( item ) {
+                        return (item.timestamp >= minuteRangeMin && item.timestamp <= minuteRangeMax);
+                    }).sort({"timestamp":-1});
+
+                    if(allMinuteItems.length == 2){
+                        diffMinuteWindowsValue = allMinuteItems[0].windowsCnt - allMinuteItems[1].windowsCnt ;
+                        diffMinuteMacValue     = allMinuteItems[0].macCnt     - allMinuteItems[1].macCnt     ;
+                        diffMinuteLinuxValue   = allMinuteItems[0].linuxCnt   - allMinuteItems[1].linuxCnt   ;
+                        diffMinuteTotalValue   = allMinuteItems[0].totalCnt   - allMinuteItems[1].totalCnt   ;
+                    }
+                }
+            }
+
+            hourItems.push({
+                timestamp         : minuteRangeMax, 
+                version           : releaseTag, 
+                minuteWindows     : diffMinuteWindowsValue, 
+                minuteMac         : diffMinuteMacValue,
+                minuteLinux       : diffMinuteLinuxValue,
+                minuteTotal       : diffMinuteTotalValue  
+            });
+        }
+
+
+        socket.emit('updateRecordHour', hourItems);
+    });
+}
+
+function sendDayValues(socket) {
+
+    var dateNow = Math.floor(Date.now()/3600000) * 3600000;
+    var timestampMin =  dateNow - (3600*25*1000);
+
+    var dayItems = [];
+
+    notesReleaseStat.find({ timestamp: { $gte: timestampMin} }).sort({"timestamp":-1}).limit(25*61).toArray(function (err, items) {
+
+        for(var i=1; i<25; i++){
+            var hourRangeMin =  dateNow - (3600*i*1000);
+            var hourRangeMax =  dateNow - (3600*(i-1)*1000);
+
+            var allHourItems;
+
+            var hourWindowsValue = 0;
+            var hourMacValue     = 0;
+            var hourLinuxValue   = 0;
+            var hourTotalValue   = 0;
+
+            if(err){
+                
+                console("** ERROR [sendDayValues]: ", err.message);
+
+            }else{
+
+                if(items.length > 0){
+
+                    allHourItems = items.filter(function( item ) {
+                        return (item.timestamp > hourRangeMin && item.timestamp <= hourRangeMax);
+                    }).sort(function(itemA,itemB){return itemB.timestamp - itemA.timestamp;});
+
+                    if(allHourItems.length > 0){
+                        hourWindowsValue = allHourItems[0].windowsCnt  - allHourItems[allHourItems.length - 1].windowsCnt ;
+                        hourMacValue     = allHourItems[0].macCnt      - allHourItems[allHourItems.length - 1].macCnt     ;
+                        hourLinuxValue   = allHourItems[0].linuxCnt    - allHourItems[allHourItems.length - 1].linuxCnt   ;
+                        hourTotalValue   = allHourItems[0].totalCnt    - allHourItems[allHourItems.length - 1].totalCnt   ;
+                    }
+                }
+            }
+
+            dayItems.push({
+                timestampHourMin : hourRangeMin, 
+                timestampHourMax : hourRangeMax,
+                version          : releaseTag, 
+                hourWindows      : hourWindowsValue, 
+                hourMac          : hourMacValue,
+                hourLinux        : hourLinuxValue,
+                hourTotal        : hourTotalValue
+            });
+        }
+
+        socket.emit('updateRecordDay', dayItems);
+        
+    });
+}
+
+db.createCollection("notesReleaseStat", {});
+var notesReleaseStat = db.collection('notesReleaseStat');
 
 db.on('error', function (err) {
     console.log('database error', err);
@@ -32,27 +236,44 @@ db.on('error', function (err) {
 
 db.on('connect', function () {
     console.log('database connected');
+    var count = 0;
+
+
+
 
     // get
-    app.get('/containerList', function(req, res){
+    app.get('/notesReleaseStat', function(req, res){
         console.log("received a GET request");
 
-        contactList.find(function(err, docs){
+        notesReleaseStat.find(function(err, docs){
             res.json(docs);
         });
     });
 
     io.on('connection', function (socket) {
-        console.log("new socket connected");
         
-        socket.on('contactAdded', function (data) {
-            console.log(JSON.stringify(data, null,'\t'));
-            if(data){
-                contactList.insert(data);
-                io.emit('updateContactAdded', data);
+        var initGraphs = function(){
+            sendTotal(socket);
+            sendHourValues(socket);
+            sendDayValues(socket);
+        }
+
+        console.log("new socket connected");
+        initGraphs();
+
+    })
+
+    setInterval(function(){
+            count++;
+            //console.log(count);
+            getDataFromGithub();
+            sendTotal(io);
+            sendHourValues(io);
+            if(count / 60 === 1){
+                count = 0;
+                sendDayValues(io);
             }
-        });
-    });
+        }, 60000);
 })
 
 
